528d085529aa5c53725b3820404018c8d9e14b00
Gideon Ritter <gideon.ritter@enymo.com>
2023-06-09 10:29:44 +0200

    Make lib null safe.

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,15 +1,15 @@
 import React, { createContext, useContext, useEffect } from "react";
 import { Socket } from "socket.io-client";
 
-const Context = createContext<Socket>(null);
+const Context = createContext<Socket | null>(null);
 
 export const SocketProvider = Context.Provider;
 export const useSocketClient = () => useContext(Context);
-export default function useSocket<T>(event: string, handler: (payload: T) => void, dependencies: React.DependencyList = []) {
+export default function useSocket<T>(event: string | undefined, handler: (payload: T) => void, dependencies: React.DependencyList = []) {
     const socket = useContext(Context);
 
     useEffect(() => {
-        if (event) {
+        if (event !== undefined) {
             socket?.on(event, handler);
             return () => {
                 socket?.off(event, handler);
