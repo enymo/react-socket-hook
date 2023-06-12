11d50db947f75f4bf76bd33548f79246cc2b9df5
Gideon Ritter <gideon.ritter@enymo.com>
2023-06-12 14:45:45 +0200

    Add documentation.

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,8 +3,17 @@ import { Socket } from "socket.io-client";
 
 const Context = createContext<Socket | null>(null);
 
+/**
+ * The application needs to be wrapped with this provider for the useSocket hook to work. Requires a socket.io client instance returned by the io function.
+ */
 export const SocketProvider = Context.Provider;
 export const useSocketClient = () => useContext(Context);
+/**
+ * Listenes to the specified websocket event and calls the handler with the events payload.
+ * @param event The event to listen to
+ * @param handler The handler used to process the payload
+ * @param dependencies The handlers dependencies
+ */
 export default function useSocket<T>(event: string | null, handler: (payload: T) => void, dependencies: React.DependencyList = []) {
     const socket = useContext(Context);
 
